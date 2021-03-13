78b2cd04950e2e5e96bcfe881e0fe348b9bc25f1
Restyled.io <commits@restyled.io>
2021-03-13 14:44:25 +0000

    Restyled by autopep8

--- generate_submission.py
+++ generate_submission.py
@@ -19,8 +19,8 @@ from idao.model import SimpleConv
 from idao.utils import delong_roc_variance
 
 
-
 dict_pred = defaultdict(list)
+
 
 def make_csv(mode, dataloader, checkpoint_path, cfg):
     torch.multiprocessing.set_sharing_strategy("file_system")
@@ -36,13 +36,14 @@ def make_csv(mode, dataloader, checkpoint_path, cfg):
     for _, (img, name) in enumerate(iter(dataloader)):
         if mode == "classification":
             dict_pred["id"].append(name[0].split('.')[0])
-            output = (1 if torch.round(model(img)["class"].detach()[0][0]) == 0 else 0)
+            output = (1 if torch.round(
+                model(img)["class"].detach()[0][0]) == 0 else 0)
             dict_pred["classification_predictions"].append(output)
 
         else:
             output = model(img)["energy"].detach()
             dict_pred["regression_predictions"].append(output[0][0].item())
- 
+
 
 def main(cfg):
     PATH = path.Path(cfg["DATA"]["DatasetPath"])
@@ -61,7 +62,8 @@ def main(cfg):
             model_path = cfg["REPORT"]["RegressionCheckpoint"]
 
         make_csv(mode, dl, model_path, cfg=cfg)
-    data_frame = pd.DataFrame(dict_pred, columns=["id", "classification_predictions", "regression_predictions"])
+    data_frame = pd.DataFrame(dict_pred, columns=[
+                              "id", "classification_predictions", "regression_predictions"])
     data_frame.to_csv('submission.csv', index=False, header=True)
 
 
--- idao/data_module.py
+++ idao/data_module.py
@@ -27,7 +27,8 @@ class IDAODataModule(pl.LightningDataModule):
                 target_transform=transforms.Compose(
                     [
                         lambda num: (
-                            torch.tensor([0, 1]) if num == 0 else torch.tensor([1, 0])
+                            torch.tensor(
+                                [0, 1]) if num == 0 else torch.tensor([1, 0])
                         )
                     ]
                 ),
@@ -35,39 +36,37 @@ class IDAODataModule(pl.LightningDataModule):
             )
 
         self.public_dataset = InferenceDataset(
-                    main_dir=self.data_dir.joinpath("public_test"),
-                    loader=img_loader,
-                    transform=transforms.Compose(
-                        [transforms.ToTensor(), transforms.CenterCrop(120)]
-                    ),
-                )
+            main_dir=self.data_dir.joinpath("public_test"),
+            loader=img_loader,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.CenterCrop(120)]
+            ),
+        )
         self.private_dataset = InferenceDataset(
-                    main_dir=self.data_dir.joinpath("private_test"),
-                    loader=img_loader,
-                    transform=transforms.Compose(
-                        [transforms.ToTensor(), transforms.CenterCrop(120)]
-                    ),
-                )
-
+            main_dir=self.data_dir.joinpath("private_test"),
+            loader=img_loader,
+            transform=transforms.Compose(
+                [transforms.ToTensor(), transforms.CenterCrop(120)]
+            ),
+        )
 
     def setup(self, stage=None):
         # called on every GPU
         self.train, self.val = random_split(
             self.dataset, [10000, 3404], generator=torch.Generator().manual_seed(666)
         )
-        
 
     def train_dataloader(self):
         return DataLoader(self.train, self.batch_size, shuffle=True, num_workers=4)
 
     def val_dataloader(self):
         return DataLoader(self.val, 1, num_workers=0, shuffle=False)
-    
+
     def test_dataloader(self):
         return DataLoader(
-            torch.utils.data.ConcatDataset([self.private_dataset, self.public_dataset]),
+            torch.utils.data.ConcatDataset(
+                [self.private_dataset, self.public_dataset]),
             self.batch_size,
             num_workers=0,
             shuffle=False
-            )
-
+        )

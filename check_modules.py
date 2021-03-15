fb72bf4f186c39daecab72244e3bd544e9c1345f
Abdalaziz Rashid <abdalaziz.rashid@outlook.com>
2021-03-15 17:36:59 +0300

    Add: a script to check if modules are importable

---
+++ check_modules.py
@@ -0,0 +1,28 @@
+modules = [
+    "torch",
+    "torchvision",
+    "timm",
+    "cv2",
+    "albumentations",
+    "catboost",
+    "keras",
+    "lightgbm",
+    "numba",
+    "scipy",
+    "sklearn",
+    "xgboost",
+]
+
+
+def module_exists(module_name):
+    try:
+        __import__(module_name)
+    except Exception:
+        return False
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    for module in modules:
+        print(f"{module}: {module_exists(module)}")
